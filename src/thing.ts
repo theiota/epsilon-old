e84f8594d7359d30c50e5633d460c018905fd227
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-22 14:52:44 -0500

    chore: delete thing

--- src/thing.ts
+++
@@ -1,9 +0,0 @@
-import Expression from './equations/expression'
-import Term from './symbols/term'
-import Operator from './symbols/operator'
-import Constant from './symbols/constant'
-
-let a = new Expression([new Term(Operator.Add, 3, new Constant(3))], Operator.None)
-let b = new Expression([new Term(Operator.Add, 3, new Constant(3))], Operator.None)
-
-a.add(b)
