1d90d7ab9442d97070e940580595676eb4898235
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-22 14:49:06 -0500

    docs: start of docs for expression

--- src/equations/expression.ts
+++ src/equations/expression.ts
@@ -1,35 +1,50 @@
-import NormalExpression from "../EETypes/NormalExpression";
-import Term from "../symbols/term";
-import Operator from "../symbols/operator";
-import Constant from '../symbols/constant'
+import NormalExpression from '../EETypes/NormalExpression'
+import Term from '../symbols/term'
+import Operator from '../symbols/operator'
 
+/**
+ * The base class for an Expression.
+ */
 class Expression implements NormalExpression {
-    terms: Term[];
-    operator: Operator;
-    constructor(terms: Term[], operator: Operator){
-        this.terms = terms;
-        this.operator = operator;
-    }
+  terms: Term[]
+  operator: Operator
+  constructor(terms: Term[], operator: Operator) {
+    this.terms = terms
+    this.operator = operator
+  }
 
-    add(expression: Expression): Expression {
+  /**
+   * A command to add two expressions together. Supports function chaining.
+   * @param expression The expression you wish to use the `add` command with.
+   */
+  add(expression: Expression): Expression {
+    console.log(expression)
+    return this
+  }
 
-        return this;
-    }
+  /**
+   * A command to multiply two expressions together. Supports function chaining.
+   * @param expression The expression you wish to use the `multiply` command with.
+   */
+  multiply(expression: Expression): Expression {
+    return this
+  }
 
-    multiply(expression: Expression): Expression {
+  /**
+   * A command to add two expressions together. Supports function chaining.
+   * @param expression The expression you wish to use the `divide` command with.
+   */
+  divide(expression: Expression): Expression {
+    return this
+  }
 
-        return this;
-    }
-
-
-
-    divide(expression: Expression): Expression {
-        return this;
-    }
-
-    subtract(expression: Expression): Expression {
-        return this;
-    }
+  /**
+   * A command to subtract one expression from another. Supports function chaining.
+   * @param expression The expression you wish to use the `subtract` command with.
+   */
+  subtract(expression: Expression): Expression {
+    return this
+  }
 }
 
-export default Expression;
+export default Expression
