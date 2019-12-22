42b28140515494286850008bc8c411b2ca39f54f
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-22 13:42:20 -0500

    refactor: update types
    I updated some of the requirements in the NormalExpression base interface

--- src/EETypes/NormalExpression.ts
+++ src/EETypes/NormalExpression.ts
@@ -1,10 +1,15 @@
-import Term from "../symbols/term";
-import Expression from "../equations/expression";
+import Term from '../symbols/term'
+import Expression from '../equations/expression'
 
+/**
+ * The `interface` for a normal expression. Includes basic operators and intrinsic values.
+ */
 interface NormalExpression {
-    terms: Term[];
-    multiply(expression: Expression): Expression;
-    add(expression: Expression): Expression;
-};
+  terms: Term[]
+  multiply(expression: Expression): Expression
+  divide(expression: Expression): Expression
+  add(expression: Expression): Expression
+  subtract(expression: Expression): Expression
+}
 
-export default NormalExpression;
+export default NormalExpression
