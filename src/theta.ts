97d2b8c029f792d5c46552cfac69b3627e15b256
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-28 09:36:37 -0500

    feat: export all classes
    update #2

--- src/theta.ts
+++ src/theta.ts
@@ -1,4 +1,12 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+
+import Expression from './equations/expression'
+import Constant from './symbols/constant'
+import Variable from './symbols/variable'
+import Term from './symbols/term'
+import Operator from './symbols/operator'
+import GreekLetter from './symbols/greekLetter'
+
+export { Expression, Constant, GreekLetter, Variable, Term, Operator }
