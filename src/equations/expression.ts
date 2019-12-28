68805343df3aac4be0396570e0aed5c7a9b19470
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-27 19:58:29 -0500

    feat: started refactor of term addition
    I decided that the current way we're adding terms together was not sustainable, so I've started
    redoing it in a better, cleaner way. It may require more functions, but it will yield better code in
    the end.

--- src/equations/expression.ts
+++ src/equations/expression.ts
@@ -2,13 +2,6 @@ import NormalExpression from '../EETypes/NormalExpression'
 import Term from '../symbols/term'
 import Operator from '../symbols/operator'
 
-class ParsedTerm {
-  coefficient: number
-  constructor(coefficient: number) {
-    this.coefficient = coefficient
-  }
-}
-
 /**
  * The base class for an Expression.
  */
@@ -37,19 +30,6 @@ class Expression implements NormalExpression {
    * @param expression The expression you wish to use the `add` command with.
    */
   add(expression: Expression): Expression {
-    // To find all the terms that
-    let selfTermsWithNoOperators = this.terms.map(term => {
-      return { coefficient: term.coefficient, value: term.value, exponent: term.exponent }
-    })
-
-    let newTermsWithNoOperators = expression.terms.map(term => {
-      return { coefficient: term.coefficient, value: term.value, exponent: term.exponent }
-    })
-
-    console.log(expression)
-    selfTermsWithNoOperators.forEach(term => {
-      expression.terms.find
-    })
     return this
   }
 
