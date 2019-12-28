83b9d0df2c27a91a340a0cb1f66073dc60475614
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-28 12:48:26 -0500

    feat: add strings to greek letters

--- src/symbols/greekLetter.ts
+++ src/symbols/greekLetter.ts
@@ -1,31 +1,31 @@
 /**
- * An enumerator of Greek Letters, because mathematicians happen to like Ancient Greek and _not_ English.
+ * An enumeration of Greek Letters, because mathematicians happen to like Ancient Greek and _not_ English.
  */
 enum GreekLetter {
-  alpha,
-  beta,
-  gamma,
-  delta,
-  epsilon,
-  zeta,
-  eta,
-  theta,
-  iota,
-  kappa,
-  lambda,
-  mu,
-  nu,
-  xi,
-  omicron,
-  pi,
-  rho,
-  sigma,
-  tau,
-  upsilon,
-  phi,
-  chi,
-  psi,
-  omega
+  Alpha = 'Α',
+  Beta = 'Β',
+  Gamma = 'Γ',
+  Delta = 'Δ',
+  Epsilon = 'Ε',
+  Zeta = 'Ζ',
+  Eta = 'Η',
+  Theta = 'θ',
+  Iota = 'Ι',
+  Kappa = 'Κ',
+  Lambda = 'Λ',
+  Mu = 'Μ',
+  Nu = 'Ν',
+  Xi = 'Ξ',
+  Omicron = 'Ο',
+  Pi = 'Π',
+  Rho = 'Ρ',
+  Sigma = 'σ',
+  Tau = 'Τ',
+  Upsilon = 'Υ',
+  Phi = 'Φ',
+  Chi = 'Χ',
+  Psi = 'Ψ',
+  Omega = 'Ω'
 }
 
 export default GreekLetter
