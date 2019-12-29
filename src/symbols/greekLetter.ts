b7415b65eb1cfdbc2154eb46a31f6da984c14717
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-28 20:34:03 -0500

    docs: add docs for greekletter

--- src/symbols/greekLetter.ts
+++ src/symbols/greekLetter.ts
@@ -2,29 +2,101 @@
  * An enumeration of Greek Letters, because mathematicians happen to like Ancient Greek and _not_ English.
  */
 enum GreekLetter {
+  /**
+   * A greek letter.
+   */
   Alpha = 'Α',
+  /**
+   * A greek letter.
+   */
   Beta = 'Β',
+  /**
+   * A greek letter.
+   */
   Gamma = 'Γ',
+  /**
+   * A greek letter.
+   */
   Delta = 'Δ',
+  /**
+   * A greek letter.
+   */
   Epsilon = 'Ε',
+  /**
+   * A greek letter.
+   */
   Zeta = 'Ζ',
+  /**
+   * A greek letter.
+   */
   Eta = 'Η',
+  /**
+   * A greek letter.
+   */
   Theta = 'θ',
+  /**
+   * A greek letter.
+   */
   Iota = 'Ι',
+  /**
+   * A greek letter.
+   */
   Kappa = 'Κ',
+  /**
+   * A greek letter.
+   */
   Lambda = 'Λ',
+  /**
+   * A greek letter.
+   */
   Mu = 'Μ',
+  /**
+   * A greek letter.
+   */
   Nu = 'Ν',
+  /**
+   * A greek letter.
+   */
   Xi = 'Ξ',
+  /**
+   * A greek letter.
+   */
   Omicron = 'Ο',
+  /**
+   * A greek letter.
+   */
   Pi = 'Π',
+  /**
+   * A greek letter.
+   */
   Rho = 'Ρ',
+  /**
+   * A greek letter.
+   */
   Sigma = 'σ',
+  /**
+   * A greek letter.
+   */
   Tau = 'Τ',
+  /**
+   * A greek letter.
+   */
   Upsilon = 'Υ',
+  /**
+   * A greek letter.
+   */
   Phi = 'Φ',
+  /**
+   * A greek letter.
+   */
   Chi = 'Χ',
+  /**
+   * A greek letter.
+   */
   Psi = 'Ψ',
+  /**
+   * A greek letter.
+   */
   Omega = 'Ω'
 }
 
