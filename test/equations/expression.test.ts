2bc62f0f7353126d423b255cd06eac96f91deb56
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-28 21:27:44 -0500

    test: update tests for string methods in expression
    I've added tests for the new methods in the Expression class.

--- test/equations/expression.test.ts
+++ test/equations/expression.test.ts
@@ -1,7 +1,71 @@
-import { Expression } from '../../src/epsilon'
+import { Expression, Term, Operator, Constant, Variable } from '../../src/epsilon'
+import { DistributableOperator } from '../../src/symbols/operator'
 
 describe('Basic Expression tests', () => {
   it('instantiates with a basic array', () => {
     // TODO
+  })
+
+  it('can distribute terms over a simple expression', () => {
+    let expression = new Expression(
+      [
+        new Term(
+          Operator.Add,
+          new Constant(4),
+          new Variable('x', new Constant(2)),
+          new Constant(3)
+        ),
+        new Term(Operator.Add, new Constant(4), new Variable('x', new Constant(2)), new Constant(3))
+      ],
+      Operator.Add
+    )
+
+    let distributedExpression = Expression.distributeOperators(
+      expression,
+      DistributableOperator.Subtract
+    )
+
+    let expressionToTest = new Expression(
+      [
+        new Term(
+          Operator.Subtract,
+          new Constant(4),
+          new Variable('x', new Constant(2)),
+          new Constant(3)
+        ),
+        new Term(
+          Operator.Subtract,
+          new Constant(4),
+          new Variable('x', new Constant(2)),
+          new Constant(3)
+        )
+      ],
+      Operator.Add
+    )
+
+    expect(distributedExpression).toEqual(expressionToTest)
+  })
+
+  it('can transfer a simple expression to a string', () => {
+    let expression = new Expression(
+      [
+        new Term(
+          Operator.Add,
+          new Constant(4),
+          new Variable('x', new Constant(2)),
+          new Constant(3)
+        ),
+        new Term(
+          Operator.Add,
+          new Constant(4),
+          new Variable('x', new Constant(2)),
+          new Constant(3)
+        ),
+        new Term(Operator.Add, new Constant(4), new Variable('x', new Constant(2)), new Constant(3))
+      ],
+      Operator.Add
+    )
+
+    expect(expression.selfToString()).toBe('+(+4(x^2)^3+4(x^2)^3+4(x^2)^3)')
   })
 })
