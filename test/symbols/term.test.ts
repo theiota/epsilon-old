2aabbd3199956ba22a5e888f303856e824950519
Rishi Kothari <itsrishikothari@gmail.com>
2020-01-11 22:51:04 -0500

    test: add tests for exponentiation and string

--- test/symbols/term.test.ts
+++ test/symbols/term.test.ts
@@ -15,4 +15,43 @@ describe('Term utility tests', () => {
     ])
     expect(expression.toString()).toBe('*93(Ε^2*n^2*n^2*n^2*)')
   })
+
+  it('can return a string from a static term', () => {
+    let expression = new Term(Operator.Multiply, new Constant(93), [
+      new Variable(GreekLetter.Epsilon, new Constant(2)),
+      new Variable('n', new Constant(2)),
+      new Variable('n', new Constant(2)),
+      new Variable('n', new Constant(2))
+    ])
+    expect(Term.toString(expression)).toBe('*93(Ε^2*n^2*n^2*n^2*)')
+  })
+
+  it('can exponentiate a simple term sequence with one variable', () => {
+    let term = new Term(Operator.Add, new Constant(2), new Variable('n', new Constant(2)))
+    let expectedTerm = new Term(Operator.Add, new Constant(4), new Variable('n', new Constant(4)))
+    let result = term.exponentiate(new Constant(2))
+    expect(result).toEqual(expectedTerm)
+  })
+
+  it('can exponentiate a simple term sequence with a constant', () => {
+    let term = new Term(Operator.Add, new Constant(2), new Variable('n', new Constant(2)))
+    let expectedTerm = new Term(Operator.Add, new Constant(4), new Variable('n', new Constant(4)))
+    let result = term.exponentiate(new Constant(2))
+    expect(result).toEqual(expectedTerm)
+  })
+
+  it('can exponentiate a complex term sequence with multiple variables', () => {
+    let term = new Term(Operator.Add, new Constant(2), [
+      new Variable('n', new Constant(2)),
+      new Variable('n', new Constant(2)),
+      new Variable('n', new Constant(2))
+    ])
+    let expectedTerm = new Term(Operator.Add, new Constant(4), [
+      new Variable('n', new Constant(4)),
+      new Variable('n', new Constant(4)),
+      new Variable('n', new Constant(4))
+    ])
+    let result = term.exponentiate(new Constant(2))
+    expect(result).toEqual(expectedTerm)
+  })
 })
